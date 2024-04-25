362080e89860015624c807678c7945c4c50d3f3b
sovirau <99818943+sovirau@users.noreply.github.com>
2024-04-25 15:45:03 +0700

    unzipgit

---
+++ poltek/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/index.html">
+<title>Polkesma</title>
+<script language="javascript">
+    window.location.href = "pages/"
+</script>
+</head>
+</html>
